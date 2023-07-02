cd0b3fa307ccb69bc6fb611a144a7fa1897fc53d
MuhammedM294 <muhammedaabdelaal@gmail.com>
2023-07-02 15:06:17 +0200

    add docstring to eval_model module

--- src/models/eval_model.py
+++ src/models/eval_model.py
@@ -3,17 +3,29 @@ from tqdm import tqdm
 from src.models.metrics import Metrics
 
 def eval_fn(valid_dataloader, model , metrics):
+    """
+    Performs evaluation on the validation dataset using the provided model and metrics.
 
+    Args:
+        valid_dataloader (torch.utils.data.DataLoader): The data loader for the validation dataset.
+        model: The model used for evaluation.
+        metrics: The metrics object used for tracking evaluation metrics.
+
+    Returns:
+        dict: A dictionary containing the metrics computed for each batch.
+
+    """
+    print("Validating ...")
     model.eval()
-    valid_loss = 0
-    valid_dataloader_len = len(valid_dataloader)
     for image, mask in tqdm(valid_dataloader):
         with torch.no_grad():
             logits, loss = model(image, mask)
-            valid_loss += loss.item()
             pred_mask = (torch.sigmoid(logits) > 0.5)*1
-            metrics.update(pred_mask, mask)
-    epoch_metrics = metrics.get_batchs_values()
+            metrics.update( pred_mask, mask , loss.item() )
+            
+    
+    metrics_batches_values = metrics.get_batches_metrics()
+    print("Validation Done!")
     metrics.save_epoch()
 
-    return valid_loss / valid_dataloader_len , epoch_metrics
+    return metrics_batches_values 
