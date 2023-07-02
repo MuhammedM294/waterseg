4d5c8062a1093bd5b6be7d66810c40163f764113
MuhammedM294 <muhammedaabdelaal@gmail.com>
2023-07-02 15:11:16 +0200

    add docstring to the unet class

--- src/models/model.py
+++ src/models/model.py
@@ -4,6 +4,20 @@ import torch.nn as nn
 
 
 class UNet(nn.Module):
+    """
+    UNet model for image segmentation.
+
+    Args:
+        encoder_name (str): The name of the encoder backbone architecture.
+        encoder_weights (str): Path to the pretrained encoder weights file or 'imagenet' for ImageNet pretraining.
+        in_channels (int, optional): Number of input channels. Defaults to 1.
+        classes (int, optional): Number of classes to predict. Defaults to 1.
+        activation (str, optional): Activation function to use. Defaults to None.
+
+    Attributes:
+        model: The UNet model from the segmentation_models.pytorch library.
+
+    """
     
     def __init__(self, encoder_name:str, encoder_weights:str,in_channels:int = 1, classes:int = 1, activation:str = None):
         
@@ -17,7 +31,18 @@ class UNet(nn.Module):
                                activation=activation)
     
     def forward(self, images, masks = None):
+        """
+        Forward pass of the UNet model.
 
+        Args:
+            images: The input images.
+            masks: The ground truth masks for training.
+
+        Returns:
+            logits: The predicted logits from the UNet model.
+            loss: The calculated loss (if ground truth masks are provided).
+
+        """
 
         logits = self.model(images)
         if masks != None:
