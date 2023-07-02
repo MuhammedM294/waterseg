ce914d59fc8157e03d9650ec01a58a8f6ad14daa
MuhammedM294 <muhammedaabdelaal@gmail.com>
2023-07-02 15:07:31 +0200

    add docstring to training model module

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -1,21 +1,37 @@
 import torch.optim as optim
 from tqdm import tqdm
-from src.models.metrics import Metrics
 import torch
+from src.models.metrics import Metrics
 
 
 def train_fn(train_dataloader, model, optimizer , metrics):
-   
+    """
+    Performs training on the training dataset using the provided model, optimizer, and metrics.
+
+    Args:
+        train_dataloader (torch.utils.data.DataLoader): The data loader for the training dataset.
+        model: The model used for training.
+        optimizer: The optimizer used for updating the model parameters.
+        metrics: The metrics object used for tracking training metrics.
+
+    Returns:
+        dict: A dictionary containing the metrics computed for each batch.
+
+    """
+
+    print("Training ...")
     model.train()
-    train_loss = 0
     for image, mask in tqdm(train_dataloader):
         optimizer.zero_grad()
         logits, loss = model(image, mask)
         pred_mask = (torch.sigmoid(logits) > 0.5)*1
-        metrics.update(pred_mask, mask)
         loss.backward()
         optimizer.step()
-        train_loss += loss.item()
-    epoch_metrics = metrics.get_batchs_values()
+        metrics.update( pred_mask, mask, loss.item())
+        
+    
+    metrics_batches_values = metrics.get_batches_metrics()
+    print("Training Done!")
     metrics.save_epoch()
-    return train_loss / len(train_dataloader) , epoch_metrics
+   
+    return  metrics_batches_values
